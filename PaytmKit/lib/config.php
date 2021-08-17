037e7796ba0d26a0c9a90b7833b0daf92696ae1d
rahulprasad2000 <rahulprasad4262@gmail.com>
2021-08-17 21:13:39 +0530

    new project

---
+++ PaytmKit/lib/config.php
@@ -0,0 +1,6 @@
+<?php
+define('MERCHANT_KEY','yme0h7sqd&7TQZCK');
+define('MERCHANT_MID','JNbtWV78121159201476');
+define('ENV','TEST');
+
+?>
---
+++ about.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+include ("partials/head.php");
+?>
+<body class="animsition">
+	<?php
+	include ("partials/header.php");
+
+
+
+?>
+
+	<!-- Title page -->
+	<section class="bg-img1 txt-center p-lr-15 p-tb-92" style="background-image: url('images/about1.png');">
+		<h2 class="ltext-105 cl0 txt-center">
+			About
+		</h2>
+	</section>	
+
+
+	<!-- Content page -->
+	<section class="bg0 p-t-75 p-b-120">
+		<div class="container">
+			<div class="row p-b-148">
+				<div class="col-md-7 col-lg-8">
+					<div class="p-t-7 p-r-85 p-r-15-lg p-r-0-md">
+						<h3 class="mtext-111 cl2 p-b-16">
+							Our Story
+						</h3>
+
+						<p class="stext-113 cl6 p-b-26">
+							I started this company to share our ideas of selling covid 19 essentials products and provide you with the best price possible. I started in 2021 and since then I have grown so much because you are our customer and you give us money to keep our company running.
+						</p>
+
+						<p class="stext-113 cl6 p-b-26">
+							My customers are our top priority and we do not want to lose any of you. So please drive safe and never text while on the wheel. It is dangerous.
+						</p>
+
+						<p class="stext-113 cl6 p-b-26">
+							My Office,Graphic era University,Pincode 832107,Dehradun or call us on (+91) 9341470813
+						</p>
+					</div>
+				</div>
+
+				<div class="col-11 col-md-5 col-lg-4 m-lr-auto">
+					<div class="how-bor1 ">
+						<div class="hov-img0">
+							<img src="images/abouty3.jpg" alt="IMG">
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<div class="row">
+				<div class="order-md-2 col-md-7 col-lg-8 p-b-30">
+					<div class="p-t-7 p-l-85 p-l-15-lg p-l-0-md">
+						<h3 class="mtext-111 cl2 p-b-16">
+							Our Mission
+						</h3>
+
+						<p class="stext-113 cl6 p-b-26">
+							Our mission is simple. Provide you with the cheapest and endurable mask and keep doing it till you are addicted to our brand and also this pandemic is over. That way, you will never go back to any other brand.
+						</p>
+
+						<div class="bor16 p-l-29 p-b-9 m-t-22">
+							<p class="stext-114 cl6 p-r-40 p-b-11">
+								After this, we will be able to control all the clothing industry like windows control the IT industry.
+							</p>
+
+							<span class="stext-111 cl8">
+								- Rahul
+							</span>
+						</div>
+					</div>
+				</div>
+
+				<div class="order-md-1 col-11 col-md-5 col-lg-4 m-lr-auto p-b-30">
+					<div class="how-bor2">
+						<div class="hov-img0">
+							<img src="images/abouty2.jpg" alt="IMG">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>	
+	
+		
+
+	<!-- Footer -->
+	<?php
+	include('partials/footer.php');
+	?>
+	
+</body>
+</html>
